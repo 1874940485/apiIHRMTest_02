30f75b1c07a4d16aeb99dfb617d3fbd11d52d6ed
cs <ch@test.com>
2020-01-08 12:03:28 +0800

    二次修改

--- run_suite.py
+++ run_suite.py
@@ -15,9 +15,8 @@ suite.addTest(unittest.makeSuite(TestEmp))
 # 员工登录模块测试套件
 # suite.addTest(unittest.makeSuite(TestLoginScript))
 # 确认测试报告路径和报告名称
-report_path = BASE_DIR + "/report/{}.html".format(time.strftime("%Y-%m-%d_%H-%M-%S"))
+report_path = BASE_DIR + "/report/ihrm.html"
 # 运行测试用例并生成测试报告
-# with open(report_path, mode="wb") as f:
-#     runner = HTMLTestRunner(stream=f, verbosity=1, title="IHRM登录接口测试", description="开发语言:Python,版本:V1.0")
-#     runner.run(suite)
-unittest.TextTestRunner().run(suite)
+with open(report_path, mode="wb") as f:
+    runner = HTMLTestRunner(stream=f, verbosity=1, title="IHRM登录接口测试", description="开发语言:Python,版本:V1.0")
+    runner.run(suite)
